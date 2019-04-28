71702500db67b8388ac1cb391f0356a71a104cae
muizzzafar11 <43075405+muizzzafar11@users.noreply.github.com>
2019-04-27 22:09:40 -0400

    Initial Commit

---
+++ Assets/MoveTargetPOs.cs
@@ -0,0 +1,32 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTarget : MonoBehaviour
+{
+
+    public LayerMask hitLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //if the player has left clicked
+        {
+            Vector3 mouse = Input.mousePosition;//get the mouse position
+            Ray castPoint = Camera.main.ScreenPointToRay(mouse);//cats a ray to where the mouse is
+            RaycastHit hit;//stores the position where the ray hits
+            if (Physics.Raycast(castPoint, out hit, Mathf.Infinity, hitLayer))//if the raycast doesn't hit a wall
+            {
+                this.transform.position = hit.point;//move the traget to the mouse position
+            }
+
+
+        }
+        
+    }
+}
---
+++ Assets/Node.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node 
+{
+    public int gridX, gridY;
+    public bool IsWall;
+    public Vector3 Position;
+    public Node Parent;
+
+}
